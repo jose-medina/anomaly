c12d75bcf9c43796c80aa193003a2c26bc16ae30
tostaconaceite <josmedbur@alum.us.es>
2013-04-10 11:15:06 +0200

    added password attributes to user and home#index created

--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,4 +1,7 @@
 Project::Application.routes.draw do
+  
+  get "home/index"
+
   resources :users
 
 
@@ -51,7 +54,7 @@ Project::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
