c5ad6c6135a0f5cd8aa8b8877c60a2006ccf6fef
tostaconaceite <josmedbur@alum.us.es>
2013-04-10 11:35:31 +0200

    dynamic-form gem and new user form added

--- project/app/controllers/users_controller.rb
+++ project/app/controllers/users_controller.rb
@@ -25,11 +25,6 @@ class UsersController < ApplicationController
   # GET /users/new.json
   def new
     @user = User.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
   end
 
   # GET /users/1/edit
@@ -41,15 +36,10 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(params[:user])
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created, location: @user }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      redirect_to root_url, :notice => "Signed up!"
+    else
+      render "new"
     end
   end
 
