96933ccf8771f6ccb72be3ad85fcee5191ee286b
tostaconaceite <josmedbur@alum.us.es>
2013-04-10 14:41:36 +0200

    session controller created. Log in, sign up and log out works

--- project/app/models/user.rb
+++ project/app/models/user.rb
@@ -1,3 +1,26 @@
 class User < ActiveRecord::Base
-  attr_accessible :email
+  require 'bcrypt'
+  attr_accessible :password, :email, :password_confirmation
+  attr_accessor :password
+  before_save :encrypt_password
+
+  validates_confirmation_of :password
+  validates :password, :presence => true, :on => :create
+  validates :email, :presence => true, :uniqueness => true
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+  end
+
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
+      user
+    else
+      nil
+    end
+  end
 end
--- project/config/routes.rb
+++ project/config/routes.rb
@@ -1,7 +1,16 @@
 Project::Application.routes.draw do
   
+  get "log_in" => "sessions#new", :as => "log_in"
+
+  get "log_out" => "sessions#destroy", :as => "log_out"
+  
+  get "sign_up" => "users#new", :as => "sign_up"
+
+  root :to => "users#new"
+
   resources :users
 
+  resources :sessions
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +61,6 @@ Project::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
